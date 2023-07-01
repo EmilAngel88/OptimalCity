d5e832188939d401db137c3e7900c12de00af38e
emil <e.baraboshkin@idaproject.com>
2023-07-01 21:08:34 +0300

    ede

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'new deploy'
+git push -f https://github.com/EmilAngel88/optimalcity.git master:gh-pages
+
+cd -
