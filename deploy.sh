48453f323ea442734808af677516e73c4ed3c521
emil <e.baraboshkin@idaproject.com>
2023-07-01 21:19:33 +0300

    edes

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init
 git add -A
 git commit -m 'new deploy'
-git push -f https://github.com/EmilAngel88/optimalcity.git master:gh-pages
+git push -f https://github.com/EmilAngel88/optimalcity.git master:optimalcity
 
 cd -
